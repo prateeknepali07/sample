bce065de4d5098945e7220d5966f52800aff195e
AkShay Mahajan <vomaksh@gmail.com>
2018-10-12 02:13:23 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homePage/HomePage";
-
+import About from "./pages/about/About"
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={About} />
+        <div>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={About} />
+        </div>
       </BrowserRouter>
     );
   }
